05842cbca9ccddd00033e4a27f350c197a518ca1
miguelax <mellesa@uninorte.edu.co>
2026-04-07 21:39:25 -0500

    Seleccionador de PJ

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -3,15 +3,27 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 var  movimiento = PlayerInput
-var skin = null 
+const SKIN_FRAMES := {
+	"idle (1)": preload("res://assets/images/sprites/idle.tres"),
+	"idle pj2": preload("res://assets/images/sprites/idlepj2.tres")
+}
+
+func _ready() -> void:
+	set_skin(Gamemanager.selectedskin)
+
 
 
 func _physics_process(delta) -> void :
 	var direction = Vector2.ZERO
 	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	velocity = direction.normalized() * SPEED	
 	move_and_slide()
 	print(direction)
 	print(position)
+	
+func set_skin(skinname): 
+	if SKIN_FRAMES.has(skinname):
+		$Animated.sprite_frames = SKIN_FRAMES[skinname]
+	$Animated.play("Idle")
 	
--- assets/Scripts/select_pj.gd
+++ assets/Scripts/select_pj.gd
@@ -1,6 +1,7 @@
 extends Control
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,6 +12,22 @@ func _process(delta: float) -> void:
 	pass
 
 
+	
+
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/Scenes/main_menu.tscn")
 	pass # Replace with function body.
+
+
+func selectedskin(skinname) -> void:
+	Gamemanager.selectedskin = skinname
+	get_tree().change_scene_to_file("res://assets/Scenes/Map.tscn")
+
+func _on_button_pressed() -> void:
+	selectedskin("idle (1)")
+	
+
+
+func _on_button_6_pressed() -> void:
+	selectedskin("idle pj2")
+	
